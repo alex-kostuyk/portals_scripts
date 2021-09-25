e8747f008dda025ad622e6bd82a010efb285bc5c
alex-kostuyk <75908349+alex-kostuyk@users.noreply.github.com>
2021-09-25 14:23:07 +0300

    Add files via upload

---
+++ Scripts/MainCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] private Portal[] _portals;
+
+    private Camera _myCamera;
+
+    private void Awake()
+    {
+        _myCamera = GetComponent<Camera>();
+    }
+
+    private void OnPreRender()
+    {
+        for (int i = 0; i < _portals.Length; i++)
+        {
+            _portals[i].Render(_myCamera);
+        }
+    }
+}
